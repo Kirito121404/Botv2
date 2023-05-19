59284db88db0004d799ff99ad179db0c4eece851
Kirito121404 <130658059+Kirito121404@users.noreply.github.com>
2023-05-19 11:38:03 +0800

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,88 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #008080;
+  color: #FFF;
+  padding: 10px;
+}
+
+h1 {
+  font-size: 24px;
+  margin: 0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #F8F8F8;
+  border-bottom: 1px solid #E5E5E5;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: #000;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #DDD;
+}
+
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.video-card {
+  width: calc(33.33% - 20px);
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #FFF;
+  border-radius: 4px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+
+.video-card img {
+  width: 100%;
+  height: auto;
+  border-radius: 4px;
+}
+
+.video-card h2 {
+  font-size: 16px;
+  margin: 10px 0;
+}
+
+.video-card p {
+  font-size: 14px;
+  color: #555;
+}
+
+.play-button {
+  cursor: pointer;
+  padding: 10px;
+  background-color: #008080;
+  color: #FFF;
+  text-align: center;
+  border-radius: 4px;
+  margin-top: 20px;
+}
